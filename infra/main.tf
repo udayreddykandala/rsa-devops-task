336cdc0b90fc3758cb76e887e5e9a30eecfd7b80
udayreddykandala <136275899+udayreddykandala@users.noreply.github.com>
2025-09-26 18:31:05 +0100

    Remove duplicate outputs from main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -168,22 +168,3 @@ resource "azurerm_virtual_machine_extension" "iis" {
 }
 SETTINGS
 }
-
-# ----------------------------
-# Outputs
-# ----------------------------
-output "lb_public_ip" {
-  value = data.azurerm_public_ip.lb_pip.ip_address
-}
-
-output "rdp_endpoints" {
-  value = {
-    vm0 = "RDP -> ${data.azurerm_public_ip.lb_pip.ip_address}:50001"
-    vm1 = "RDP -> ${data.azurerm_public_ip.lb_pip.ip_address}:50002"
-  }
-}
-
-output "admin_credentials" {
-  value     = { username = var.admin_username, password = random_password.admin.result }
-  sensitive = true
-}
